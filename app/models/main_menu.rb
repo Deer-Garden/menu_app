d37112f15da30544ee0dd038c697de2e7f93fc1d
Deer-Garden <willkinsongs.alais@gmail.com>
2021-02-10 17:01:00 +0900

    編集削除ボタンのビュー

--- app/models/main_menu.rb
+++ app/models/main_menu.rb
@@ -1,7 +1,7 @@
 class MainMenu < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
